307dd58a3c9f521bb60cadd75be0ae5f13f7bfdd
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-02-06 18:46:35 +0900

    상대 경로로 수정.

--- member_insert.php
+++ member_insert.php
@@ -26,7 +26,7 @@
     echo "
         <script>
             alert('회원가입이 완료되었습니다.');
-            location.href = 'index.php';
+            location.href = './index.php';
         </script>
         ";
     ?>
