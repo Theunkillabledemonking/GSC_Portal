12e97bcf9cb309cec5fbdac6cb4812c8ae01931e
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-02-07 17:43:42 +0900

    권한 수정 오류 해결

--- member_modify.php
+++ member_modify.php
@@ -3,15 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// ↓↓↓ 디버깅용 코드 추가 ↓↓↓
-echo "<pre>";
-echo "디버그: POST 데이터\n";
-var_dump($_POST);
-echo "\n디버그: GET 데이터\n";
-var_dump($_GET);
-echo "</pre>";
-// ↑↑↑ 디버깅용 코드 ↑↑↑
-
 // GET으로 id 받기
 $id = $_GET['id'];
 
@@ -30,8 +21,7 @@ $sql = "UPDATE members SET pass='$pass', role='$role' WHERE id='$id'";
 echo "id 값 확인: $id<br>";
 if (mysqli_query($con, $sql)) {
     echo "업데이트 성공!<br>";
-    // ※ 주의: header 전에 echo를 하면 'headers already sent' 경고가 뜰 수 있음
-    //header("Location: index.php");
+    header("Location: index.php");
     exit; // 헤더 후에는 exit 권장
 } else {
     echo "Error updating record: " . mysqli_error($con);
