1830ab85ba7c2a4996770961f84ca81166c0e7f9
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-02-07 16:47:02 +0900

    html 구조 유지 보수

--- member_modify_form.php
+++ member_modify_form.php
@@ -1,5 +1,6 @@
-<scrit type="text/javascript" src="./js/member_form.js"></scrit>
+
 <?php
+    echo "디버깅 시작";
     session_start();
 
     $userid = isset($_SESSION['userid']) ? $_SESSION['userid'] : "";
@@ -7,13 +8,24 @@
     $role = isset($_SESSION['role']) ? $_SESSION['role'] : "";
 
     $con = mysqli_connect("localhost", "root", "gsc1234!@#$", "school_portal");
+    if(!$con) {
+        die("DB 연결 실패: " . mysqli_connect_error());
+    }
+
     $sql = "select * from members where id='$userid'";
     $result = mysqli_query($con, $sql);
-    $row = mysqli_fetch_array($result);
 
-    $pass = $row["pass"];
-    $name = $row["name"];
-    $role = $row["role"];
+    if ($result) {
+        $row = mysqli_fetch_array($result);
+
+        $pass = $row["pass"];
+        $name = $row["name"];
+        $role = $row["role"];
+        echo "데이터베이스에서 데이터 가져오기 성공!";
+    } else {
+        echo "데이터베이스 쿼리 실패:" . mysqli_error($con);
+    }
+
 
     mysqli_close($con);
 ?>
@@ -35,19 +47,19 @@
                 <!-- 아이디 -->
                 <div class="form id">
                     <div>아이디</div>
-                    <div><?=$userid?></div>
+                    <div><?= htmlspecialchars($userid, ENT_QUOTES, 'UTF-8')?></div>
                 </div>
 
                 <!-- 비밀번호 -->
                 <div class="form">
                     <div>비밀번호</div>
-                    <div><input type="password" name="pass" id="pass" value="<?=$pass?>"></div>
+                    <div><input type="password" name="pass" id="pass" value="<?=htmlspecialchars($pass, ENT_QUOTES, 'UTF-8')?>"></div>
                 </div>
 
                 <!-- 비밀번호 확인 -->
                 <div class="form">
                     <div>비밀번호 확인</div>
-                    <div><input type="password" name="pass_confirm" value="<?=$pass?>"></div>
+                    <div><input type="password" name="pass_confirm" value="<?=htmlspecialchars($pass, ENT_QUOTES, 'UTF-8') ?>"></div>
                 </div>
 
                 <!-- 권한 수정 -->
@@ -72,9 +84,6 @@
     </div>
 </section>
 
-<!--<footer>-->
-<!--    --><?php //include "footer.php"; ?>
-<!--</footer>-->
 </body>
 </html>
 
