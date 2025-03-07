4d69339a4d0878cbbeb37f60a066ba05c136e6fc
Theunkillabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-03-08 01:28:36 +0900

    라우터 등록

--- my-vue-app/google-oauth-mysql/backend/routes/timetableRoutes.js
+++ my-vue-app/google-oauth-mysql/backend/routes/timetableRoutes.js
@@ -2,13 +2,22 @@ const express = require('express');
 const router = express.Router();
 const timetableController = require('../controllers/timetableController');
 
-// 정규 시간표 + 이벤트 조회 (학기별, 학년별 조회)
+// 정규 시간표 등록
+router.post('/timetable', timetableController.createTimetable);
+
+// 정규 시간표 수정
+router.put('/timetable/:id', timetableController.updateTimetable);
+
+// 정규 시간표 삭제
+router.delete('/timetable/:id', timetableController.deleteTimetable);
+
+// 학기별 시간표 + 이벤트 조회
 router.get('/timetable-with-events', timetableController.getTimetableWithEvents);
 
-// 이벤트 등록 (휴강, 보강, 특강)
+// 이벤트 등록
 router.post('/events', timetableController.createEvent);
 
-// 이벤트 수정 (휴강, 보강, 특강)
+// 이벤트 수정
 router.put('/events/:event_id', timetableController.updateEvent);
 
 // 이벤트 삭제
