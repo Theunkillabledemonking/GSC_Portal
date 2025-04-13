2369d6d4f7382ac3d272deac1a51c34a7fcbc49e
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-04-14 02:29:10 +0900

    frontend: 의존성 및 Vite 설정 업데이트

--- my-vue-app/google-oauth-mysql/frontend/vite.config.js
+++ my-vue-app/google-oauth-mysql/frontend/vite.config.js
@@ -7,18 +7,25 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src')
+            '@': path.resolve(__dirname, './src'),
+            '@store': path.resolve(__dirname, 'src/store'),
+            '@modules': path.resolve(__dirname, 'src/store/modules'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@views': path.resolve(__dirname, 'src/views'),
+            '@services': path.resolve(__dirname, 'src/services'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@constants': path.resolve(__dirname, 'src/constants'),
+            '@assets': path.resolve(__dirname, 'src/assets')
         }
     },
     server: {
+        port: 5173,
         proxy: {
             '/api': {
                 target: 'http://localhost:5000',
                 changeOrigin: true,
-                secure: false,
             }
         },
-        port: 5173,
         open: true,
     },
     define: {
